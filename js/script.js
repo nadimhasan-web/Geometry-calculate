4a01c04a4f7d3b0a9f0f417056700b92113497dc
nadim <nadimhasannayan77@gmail.com>
2024-02-12 23:55:31 +0600

    geometry

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function calculate(){
+    // base input
+    const inputBase = document.getElementById('input_base');
+    const baseValue = inputBase.value;
+    const base = parseFloat(baseValue);
+    // height input
+    const inputHeight = document.getElementById('input_height');
+    const heightValue = inputHeight.value;
+    const height = parseFloat(heightValue);
+    // calculate result
+    const ressult = 0.5 * base * height;
+    // show result
+    const resultValue = document.getElementById('area_output');
+    resultValue.innerText = ressult;
+    console.log(resultValue);
+}
